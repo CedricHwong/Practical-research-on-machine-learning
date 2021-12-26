a5c89dcfb5607c634ed99a2da37ed76df037753b
Cedric Hwong <cedrichwong@CedricdeMacBook-Pro.local>
2021-12-25 22:54:54 -0800

    DecisionTree & some update

---
+++ DecisionTree/lensTypePredict.py
@@ -0,0 +1,12 @@
+import trees
+import treePlotter
+
+fr = open('lenses.txt')
+
+lenses =  [inst.strip().strip().split('\t') for inst in fr.readlines()]
+
+lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+
+lensesTree = trees.createTree(lenses, lensesLabels)
+
+treePlotter.createPlot(lensesTree)
