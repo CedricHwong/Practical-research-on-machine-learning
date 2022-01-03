5f04fb785ea5d249c1395eb732d9169731dcc13f
Cedric Hwong <cedrichwong@CedricdeMacBook-Pro.local>
2022-01-02 18:52:30 -0800

    SVM update

---
+++ SVM/HandwritingProblemReview.py
@@ -0,0 +1,55 @@
+'''
+Created on Dec 30, 2021
+
+@author: Cedric
+'''
+
+
+import svmMLiA
+from numpy import *
+
+
+
+
+def loadImages(dirName):
+    from os import listdir
+    hwLabels = []
+    trainingFileList = listdir(dirName)           #load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        if classNumStr == 9: hwLabels.append(-1)
+        else: hwLabels.append(1)
+        trainingMat[i,:] = svmMLiA.img2vector('%s/%s' % (dirName, fileNameStr))
+    return trainingMat, hwLabels    
+
+def testDigits(kTup=('rbf', 10)):
+    dataArr,labelArr = loadImages('digits/trainingDigits')
+    b,alphas = svmMLiA.smoP(dataArr, labelArr, 200, 0.0001, 10000, kTup)
+    datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
+    svInd=nonzero(alphas.A>0)[0]
+    sVs=datMat[svInd] 
+    labelSV = labelMat[svInd];
+    print( "there are %d Support Vectors" % shape(sVs)[0])
+    m,n = shape(datMat)
+    errorCount = 0
+    for i in range(m):
+        kernelEval = svmMLiA.kernelTrans(sVs,datMat[i,:],kTup)
+        predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+        if sign(predict)!=sign(labelArr[i]): errorCount += 1
+    print("the training error rate is: %f" % (float(errorCount)/m)) 
+    dataArr,labelArr = loadImages('digits/testDigits')
+    errorCount = 0
+    datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
+    m,n = shape(datMat)
+    for i in range(m):
+        kernelEval = svmMLiA.kernelTrans(sVs,datMat[i,:],kTup)
+        predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+        if sign(predict)!=sign(labelArr[i]): errorCount += 1    
+    print("the test error rate is: %f" % (float(errorCount)/m) ) 
+
+
+testDigits(('rbf', 20))
